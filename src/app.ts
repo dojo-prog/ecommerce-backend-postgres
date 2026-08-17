7ed75214816278c89cb4a4debd99a20ceddba3a0
dojo <djdumaluan.prog@gmail.com>
2026-08-17 14:52:49 +0800

    feat: register auth router

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import errorMiddleware from "./middlewares/error.middleware";
 import cookieParser from "cookie-parser";
 
+import authRouter from "./routers/auth.routes";
+
 const app = express();
 
 // Parsers
@@ -9,6 +11,7 @@ app.use(express.json({ limit: "1mb" }));
 app.use(cookieParser());
 
 // Routers
+app.use("/api/v1/auth", authRouter);
 
 // Error Handler
 app.use(errorMiddleware);
