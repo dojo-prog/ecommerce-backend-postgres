09698da97fc87f2991aaca4f23b3a18a1fe65ecc
dojo <djdumaluan.prog@gmail.com>
2026-08-18 05:36:59 +0800

    chore: init main seed handler

---
+++ seed/index.ts
@@ -0,0 +1,18 @@
+import pool from "../src/database/db";
+import seedUsers from "./users/seed";
+
+const startSeed = async () => {
+  try {
+    await pool.query("BEGIN");
+
+    await seedUsers();
+
+    await pool.query("COMMIT");
+    console.log("\nSeeding complete.\n");
+  } catch (error) {
+    await pool.query("ROLLBACK");
+    console.error("An error has occured while seeding", error);
+  }
+};
+
+startSeed();
