148a71779f0b77f8c86ce8ffcf9051ebaf554a14
dojo <djdumaluan.prog@gmail.com>
2026-08-20 09:57:30 +0800

    feat: register payment router

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ import productRouter from "./routers/product.routes";
 import inventoryRouter from "./routers/inventory.routes";
 import cartItemRouter from "./routers/cart_item.routes";
 import orderRouter from "./routers/order.routes";
+import paymentRouter from "./routers/payment.routes";
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.use("/api/v1/products", productRouter);
 app.use("/api/v1/products", inventoryRouter);
 app.use("/api/v1/cart/items", cartItemRouter);
 app.use("/api/v1/orders", orderRouter);
+app.use("/api/v1/", paymentRouter);
 
 // Error Handler
 app.use(errorMiddleware);
