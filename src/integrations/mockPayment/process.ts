771976e2f738768b3d5e702a6daf72d609c04808
dojo <djdumaluan.prog@gmail.com>
2026-08-20 10:01:57 +0800

    refactor: separated processPayment to integrations/mockPayment

---
+++ src/integrations/mockPayment/process.ts
@@ -0,0 +1,40 @@
+import { CreatePaymentPayload } from "../../schemas/payments";
+import * as paymentModel from "../../models/payment.model";
+
+export const processPayment = async (
+  orderId: string,
+  amountCents: number,
+  shouldFail = false,
+) => {
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
+  const payload: Partial<CreatePaymentPayload> = {
+    order_id: orderId,
+    amount_cents: amountCents,
+    transaction_id: crypto.randomUUID(),
+  };
+
+  const { transaction_id } = payload;
+
+  if (shouldFail) {
+    payload.status = "failed";
+
+    await paymentModel.create(payload as CreatePaymentPayload);
+
+    return {
+      success: false,
+      transaction_id,
+    };
+  }
+
+  payload.status = "successful";
+
+  await paymentModel.create(payload as CreatePaymentPayload);
+
+  return {
+    success: true,
+    transaction_id,
+  };
+};
+
+export default processPayment;
--- src/services/payment.service.ts
+++ src/services/payment.service.ts
@@ -1,47 +1,12 @@
 import { OrderWithItems } from "../schemas/orders";
-import { CreatePaymentPayload, Payment } from "../schemas/payments";
+import { Payment } from "../schemas/payments";
 import AppError from "../utils/AppError";
 
 import * as paymentModel from "../models/payment.model";
 import * as orderModel from "../models/order.model";
 
 import * as orderService from "../services/order.service";
-
-export const processPayment = async (
-  orderId: string,
-  amountCents: number,
-  shouldFail = false,
-) => {
-  await new Promise((resolve) => setTimeout(resolve, 500));
-
-  const payload: Partial<CreatePaymentPayload> = {
-    order_id: orderId,
-    amount_cents: amountCents,
-    transaction_id: crypto.randomUUID(),
-  };
-
-  const { transaction_id } = payload;
-
-  if (shouldFail) {
-    payload.status = "failed";
-
-    await paymentModel.create(payload as CreatePaymentPayload);
-
-    return {
-      success: false,
-      transaction_id,
-    };
-  }
-
-  payload.status = "successful";
-
-  await paymentModel.create(payload as CreatePaymentPayload);
-
-  return {
-    success: true,
-    transaction_id,
-  };
-};
+import processPayment from "../integrations/mockPayment/process";
 
 export const payOrder = async (
   userId: string,
