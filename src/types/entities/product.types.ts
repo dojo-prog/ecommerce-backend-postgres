ab41fad57ac5a95373593615d93711d952ff3bec
dojo <djdumaluan.prog@gmail.com>
2026-09-01 02:53:14 +0800

    chore: add CreateProductData

--- src/types/entities/product.types.ts
+++ src/types/entities/product.types.ts
@@ -1,5 +1,17 @@
-import { ProductWithRelations } from "../../schemas/products";
+import {
+  CreateProductBody,
+  ProductWithRelations,
+} from "../../schemas/products";
 import { GetResult, UpdateResult } from "./common";
+
+// =======================================
+// REPOSITORY DATA
+// =======================================
+
+export interface CreateProductData extends CreateProductBody {
+  thumbnail_url?: string;
+  thumbnail_public_id?: string;
+}
 
 // =======================================
 // RESULT
